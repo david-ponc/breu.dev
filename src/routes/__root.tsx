2279c277e4489d0f34a0e761903cb13905d78311
David Ponce Vargas <buap.david@gmail.com>
2026-07-08 10:01:42 -0600

    feat: add tanstack start app shell with tailwind css

---
+++ src/routes/__root.tsx
@@ -0,0 +1,43 @@
+import { TanStackDevtools } from '@tanstack/react-devtools';
+import { createRootRoute, HeadContent, Scripts } from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import type { PropsWithChildren } from 'react';
+
+import appCss from '../styles.css?url';
+
+export const Route = createRootRoute({
+	head: () => ({
+		meta: [
+			{ charSet: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ title: 'TanStack Start Starter' },
+		],
+		links: [{ rel: 'stylesheet', href: appCss }],
+	}),
+	shellComponent: RootDocument,
+});
+
+function RootDocument({ children }: PropsWithChildren) {
+	return (
+		<html lang='en'>
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<TanStackDevtools
+					config={{
+						position: 'bottom-right',
+					}}
+					plugins={[
+						{
+							name: 'Tanstack Router',
+							render: <TanStackRouterDevtoolsPanel />,
+						},
+					]}
+				/>
+				<Scripts />
+			</body>
+		</html>
+	);
+}
